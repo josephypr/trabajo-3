906cf39c89e2422ab561706f00775fed71b048f9
FabianAmaya8 <ozkaritogiofabis@gmail.com>
2024-08-23 12:12:55 -0500

    multiplicacion y divicion

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+elif seleccion == 3:
+    print("multiplicacion")
+    num1=float (input("digite el primer numero"))
+    num2=float (input("digite el segundo numero"))
+    resul= num1 * num2 
+    print ("la suma es:",resul)
+    
+elif seleccion == 4:
+    print("divicion")
+    num1=float (input("digite el primer numero"))
+    num2=float (input("digite el segundo numero"))
+    resul= num1 / num2 
+    print ("la resta es:" ,resul)
